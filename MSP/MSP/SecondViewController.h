893031d810c7fc896f693c542ca920079a992c61
athirasatheesh <athirasatheesh.0017@gmail.com>
2015-10-03 14:24:08 +0530

    old version msp
    mspold

---
+++ MSP/MSP/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MSP
+//
+//  Created by athira on 03/10/15.
+//  Copyright (c) 2015 athira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ MSP/MSP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MSP
+//
+//  Created by athira on 03/10/15.
+//  Copyright (c) 2015 athira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
